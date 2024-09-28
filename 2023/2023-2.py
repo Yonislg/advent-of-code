00da46dd9934ee0af546572f0d71cc7965c50091
Yonis <yslegrand1@gmail.com>
2024-09-28 20:19:31 +0200

    sum working

--- 2023/2023-2.py
+++ 2023/2023-2.py
@@ -1,6 +1,6 @@
 import re
 f = [
-    'two1ninetwo',
+    'two1nine',
 'eightwothree',
 'abcone2threexyz',
 'xtwone3four',
@@ -18,25 +18,26 @@ def cipherize(line):
     
     return newline
 
-
+ans = 0
 for row in f:
-    print(row)
     for i in range(len(row)):
         if row[i].isdigit():
-            print(row[i])
+            dec = row[i]
             break
         elif i > 1:
             for b in range(i,i+4):
                 a = cipherize(row[i-2:b])
                 if a.isdigit():
-                    print(a)
+                    dec = a
                     break
             if a.isdigit():
                 break
 
+    ans += int(dec)*10
+
     for i in range(-1,-len(row),-1):
         if row[i].isdigit():
-            print(row[i])
+            sing = row[i]
             break
         elif i < - 2:
             for b in range(i,i-4,-1):
@@ -45,10 +46,12 @@ for row in f:
                 else:
                     a = cipherize(row[b:i+3])
                 if a.isdigit():
-                    print(a)
                     break
             if a.isdigit():
+                sing = a
                 break
-   
+    ans += int(sing)
+
+print(ans)
 
             
