90dfa1743c5b954796b0abfd283f09957a4ce0a2
Yonis <yslegrand1@gmail.com>
2024-10-07 16:06:57 +0200

    2023 day 1 with comments

--- 2023/Day-1/2023-1.py
+++ 2023/Day-1/2023-1.py
@@ -1,25 +1,37 @@
+# https://adventofcode.com/2023/day/1
+# Santa receives a code where lines contain numbers and letters.
+# The first and last digit of each line form a two digit number, the callibration value.
+# The assignment is to find this number for each line and  add them all.
+
 f = open('2023/Day-1/2023-1-input.txt','r')
 
-# f = ['1abc2',
-# 'pqr3stu8vwx',
-# 'a1b2c3d4e5f',
-# 'treb7uchet']
+test_example = ['1abc2',
+'pqr3stu8vwx',
+'a1b2c3d4e5f',
+'treb7uchet']
 
-a = 0
-for row in f:
-    for i in row:
-        if i.isdigit():
-            a += int(i)*10
-            break
+# In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 
-    for i in reversed(row):
-        if i.isdigit():
-            a += int(i)
-            break
+# The approach is to iterate forwards and backwards over each row to find the first (and last) numbers
+def num_finder(message):
+    a = 0
+    for row in message:
+        for i in row:
+            if i.isdigit():
+                a += int(i)*10
+                break
 
-print(a)
+        for i in reversed(row):
+            if i.isdigit():
+                a += int(i)
+                break
+    return a
 
+if (num_finder(test_example) == 142):
+    print('code works with example')
 
+ans = num_finder(f)
+print('The puzzle answer is:', ans)
 
 
 f.close()
