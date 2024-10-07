8b4ecbbc8041df3fa3fb5087cc601800a9552f98
Yonis <yslegrand1@gmail.com>
2024-10-07 15:57:33 +0200

    comments for 2015 challenge day 1

--- 2015/2015-1.py
+++ 2015/2015-1.py
@@ -1,4 +1,10 @@
-# Santa floor
+#  https://adventofcode.com/2015/day/1
+# Santa receiives a coded message which tells him which floor to go to. 
+# He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
+# An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor
+# To what floor do the instructions take Santa?
+
+# Open instructions
 a = open('2015/2015-1-input.txt','r')
 
 f = a.read()
@@ -7,6 +13,7 @@ a.close()
 up = f.count('(')
 down = f.count(')')
 
+# print final floor
 ans = up - down
 
 print(ans)
--- 2015/2015-2.py
+++ 2015/2015-2.py
@@ -1,4 +1,10 @@
+# https://adventofcode.com/2015/day/1
+# Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). 
+# The first character in the instructions has position 1, the second character has position 2, and so on.
+
+
 a = open('2015/2015-1-input.txt','r')
+
 
 f = a.read()
 a.close()
@@ -7,6 +13,7 @@ idx = 0
 lvl = 0
 basement = 0
 
+# A simple floor loop that keeps track of the current floor Santa is at.
 for x in f:
     idx += 1
     if x =='(':
