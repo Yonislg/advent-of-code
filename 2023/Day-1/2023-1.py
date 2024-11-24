a1921fd51b1a53ea941f3583d215b60e92e57cc5
Yonis <yslegrand1@gmail.com>
2024-11-24 21:37:00 +0100

    clean up day 1

--- 2023/Day-1/2023-1.py
+++ 2023/Day-1/2023-1.py
@@ -2,7 +2,8 @@
 # Santa receives a code where lines contain numbers and letters.
 # The first and last digit of each line form a two digit number, the callibration value.
 # The assignment is to find this number for each line and  add them all.
-
+import re
+import time
 f = open('2023/Day-1/2023-1-input.txt','r')
 
 test_example = ['1abc2',
@@ -21,17 +22,39 @@ def num_finder(message):
                 a += int(i)*10
                 break
 
-        for i in reversed(row):
-            if i.isdigit():
-                a += int(i)
+        for id in range(len(row)-1,-1,-1):
+            if row[id].isdigit():
+                a += int(row[id])
                 break
     return a
 
 if (num_finder(test_example) == 142):
     print('code works with example')
 
+start = time.perf_counter()
 ans = num_finder(f)
+end = time.perf_counter()
+t1 = end - start
 print('The puzzle answer is:', ans)
 
-
 f.close()
+
+def simple_num_finder(message):
+    a = 0
+    for row in message:
+        nums = re.findall('\d',row)
+        a+= int(nums[0]+nums[-1])
+    return a
+
+
+f = open('2023/Day-1/2023-1-input.txt','r')
+start = time.perf_counter()
+ans = simple_num_finder(f)
+end = time.perf_counter()
+print(ans)
+f.close
+
+t2 = end - start
+
+print(t1,t2,(t1-t2)/t1*100)
+
