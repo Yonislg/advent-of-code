0b25c67813583f4e47d24ac3335ce6fe3356f810
Yonis <yslegrand1@gmail.com>
2024-12-18 16:07:15 +0100

    day 7 part 1

---
+++ 2024/Day-7/2024-7-1.py
@@ -0,0 +1,43 @@
+f = open('2024/Day-7/2024-7-input.txt','r')
+puzzle_input = f.read().split('\n')
+f.close()
+
+f2 = open('2024/Day-7/example.txt','r')
+example = f2.read().split('\n')
+f2.close()
+
+def callibartion_sort(input):
+    equations = []
+    for row in input:
+        answer, eq = row.split(':')
+        eq = eq.split(' ')
+        equations.append([int(answer), [int(x) for x in eq[1:]]])
+
+    return equations
+
+def calibration_check(ans, eq):
+    for mult in range(2**(len(eq)-1)):
+        specifier = '{0:0'+str(len(eq)-1)+'b}'
+        bin_rev = specifier.format(mult)
+        bin = bin_rev[::-1]
+        sum = eq[0]
+        for ii in range(len(eq)-1):
+            if bin[ii] == '0':
+                sum += eq[ii+1]
+            else:
+                sum *= eq[ii+1]
+        if sum == ans:
+            return True
+    return False
+
+def callibrators(equations):
+    sum = 0
+    for equation in equations:
+        ans = equation[0]
+        LHS = equation[1]
+        if (calibration_check(ans,LHS)):
+            sum += ans
+    return sum
+
+equations = callibartion_sort(puzzle_input)
+print(callibrators(equations))
