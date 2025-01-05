5d23e48b94e4328b51e744898521472a5701d647
Yonis <yslegrand1@gmail.com>
2025-01-05 17:49:52 +0100

    Day 9 party 2 first attempt

---
+++ 2024/Day-9/2024-9-2.py
@@ -0,0 +1,47 @@
+example = '2333133121414131402'
+
+f = open('2024/Day-9/2024-9-input.txt','r')
+puzzle_input = f.read()
+f.close()
+
+def filenums(group,start,space):
+    end = int(space)
+    group.append([start + k for k in range(end)])
+    start += end
+    return group, start
+
+def checksum(diskmap):
+    input =[]
+    for char in diskmap:
+        input.append(int(char))
+    
+    sum = 0
+
+    blocks =[]
+    empty=[]
+    
+    j = 0
+    for i in range(len(diskmap)):
+        if i%2==0:
+            blocks, j = filenums(blocks, j, diskmap[i])
+        else:
+            empty, j = filenums(empty, j, diskmap[i])
+    
+    
+    for n in range(len(blocks)-1,-1,-1):
+        block = blocks[n]
+        potential = [space for space in empty if len(space) >= len(block)]
+        if len(potential)>0:
+            for l in range(len(block)):
+                sum += n * potential[0][0]
+                potential[0].pop(0)
+        else:
+            for l in range(len(block)):
+                sum += n * int(block[l])   
+
+                  
+            
+
+    return sum
+
+print(checksum(puzzle_input))
